d0b037b8b94cee6cb00ca45718baaaa615748442
Pedro Henrique <pedroh.tav.p@gmail.com>
2024-11-22 14:07:46 -0300

    teste

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner(){
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner"/>
+        </header>
+    );
+}
+
+export default Banner;
